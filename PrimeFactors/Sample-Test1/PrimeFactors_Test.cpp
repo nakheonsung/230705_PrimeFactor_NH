d938c8cda916c0716aa01b9d36eb09d1ecb5be6f
nakheon.sung <zlekflepqlf@gmail.com>
2023-07-05 15:14:40 +0900

    refac tc fixture

--- PrimeFactors/Sample-Test1/PrimeFactors_Test.cpp
+++ PrimeFactors/Sample-Test1/PrimeFactors_Test.cpp
@@ -4,15 +4,20 @@
 #include <vector>
 using namespace std;
 
-TEST(PrimeFactors, Of1) {
+class PrimeFactorFixture : public testing::Test
+{
+public:
     PrimeFactor prime_factor;
-    vector<int> expected = {};
+    vector<int> expected;
+};
+
+TEST_F(PrimeFactorFixture, Of1) {
+    expected = {};
     EXPECT_EQ(expected, prime_factor.of(1));
 }
 
-TEST(PrimeFactors, Of2) {
-    PrimeFactor prime_factor;
-    vector<int> expected = {2};
+TEST_F(PrimeFactorFixture, Of2) {
+    expected = {2};
     EXPECT_EQ(expected, prime_factor.of(2));
 }
 
