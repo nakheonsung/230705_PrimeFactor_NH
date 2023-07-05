6d43dd31660236c143314ce9fb33f3ebd57c492c
nakheon.sung <zlekflepqlf@gmail.com>
2023-07-05 15:25:19 +0900

    add tc of6 , refac function of

--- PrimeFactors/Sample-Test1/PrimeFactors_Test.cpp
+++ PrimeFactors/Sample-Test1/PrimeFactors_Test.cpp
@@ -31,3 +31,8 @@ TEST_F(PrimeFactorFixture, Of4) {
     EXPECT_EQ(expected, prime_factor.of(4));
 }
 
+TEST_F(PrimeFactorFixture, Of6) {
+    expected = { 2,3 };
+    EXPECT_EQ(expected, prime_factor.of(6));
+}
+
