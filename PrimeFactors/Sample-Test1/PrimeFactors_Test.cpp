79b8e002e3a0e52e2045d03d22e0caaf422d807c
nakheon.sung <zlekflepqlf@gmail.com>
2023-07-05 15:27:54 +0900

    add tc of9 , refac merge case 4 6 9 function of

--- PrimeFactors/Sample-Test1/PrimeFactors_Test.cpp
+++ PrimeFactors/Sample-Test1/PrimeFactors_Test.cpp
@@ -36,3 +36,8 @@ TEST_F(PrimeFactorFixture, Of6) {
     EXPECT_EQ(expected, prime_factor.of(6));
 }
 
+TEST_F(PrimeFactorFixture, Of9) {
+    expected = { 3,3 };
+    EXPECT_EQ(expected, prime_factor.of(9));
+}
+
